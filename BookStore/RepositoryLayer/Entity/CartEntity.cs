7136c6d1ab370d83675c02c73e8e73758aa1922b
Pujaborse13 <pujaborse13@gmail.com>
2025-05-11 01:46:24 +0530

    [Puja] ADDEd - CartEntity Migration

---
+++ BookStore/RepositoryLayer/Entity/CartEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class CartEntity
+    {
+   
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int CartId { get; set; }
+
+            [ForeignKey("Users")]
+            public int CustomerId { get; set; }  // FK to User
+
+            [ForeignKey("Book")]
+            public int BookId { get; set; }  // FK to Book
+
+            public int Quantity { get; set; }
+
+            public Decimal SinglUnitPrice { get; set; }
+
+            public bool IsPurchased { get; set; } = false;
+
+            // Navigation properties
+            public virtual UserEntity UserEntity { get; set; }
+            public virtual BookEntity BookEntity { get; set; }
+    }
+
+}
