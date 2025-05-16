bd7db4b432f345227c7c87f6d38fdcdb6f1ddd8a
Pujaborse13 <pujaborse13@gmail.com>
2025-05-16 23:52:21 +0530

    [Puja] MOD - CartEntity Migration

--- BookStore/RepositoryLayer/Migrations/20250516181326_updatedCartEntity_V2.Designer.cs
+++ BookStore/RepositoryLayer/Migrations/20250516181326_updatedCartEntity_V2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RepositoryLayer.Context;
 
 namespace RepositoryLayer.Migrations
 {
     [DbContext(typeof(BookStoreDBContext))]
-    partial class BookStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250516181326_updatedCartEntity_V2")]
+    partial class updatedCartEntity_V2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
